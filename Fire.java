4d87a41a485280609c8874cd3d5ff82d4868344b
saanyay <saanyajauhri@gmail.com>
2024-06-11 17:57:37 -0400

    Update Fire.java

--- Fire.java
+++ Fire.java
@@ -7,52 +7,22 @@ public class Fire {
     // Initializing Variables 
     private static final Random random = new Random();
 
-    private final Ship ship;
-    private final double q; //Flammability 
-    private final List<int[]> burningCells; 
+    //private final double q; //Flammability 
+    //private final List<int[]> burningCells; 
 
     // Constructor 
-    public Fire(Ship ship, double q) {
-        this.ship = ship;
-        this.q = q;
-        this.burningCells = new ArrayList<>();
+    public Fire(Ship ship) {
+        // Find a random cell and put fire in it
+        ship.setCellOnFire();
+
     }
 
-    // Finds all the open cells, starts the fire at random open cell
-    public void startFire() {
-        List<int[]> openCells = ship.getOpenCells();
-        int[] startCell = openCells.get(random.nextInt(openCells.size()));
-        burningCells.add(startCell);
-    }
-
-    // Returns a List of all openNeighbors given a position in the grid
-    public List<int[]> getOpenNeighbors(int x, int y){
-        List<int[]> openNeighbors = new ArrayList<>();
-
-        if(ship.isOpenCell(x-1,y)){
-            openNeighbors.add(new int[]{x-1,y});
-        }
-        if(ship.isOpenCell(x+1,y)){
-            openNeighbors.add(new int[]{x+1,y});
-        }
-        if(ship.isOpenCell(x,y-1)){
-            openNeighbors.add(new int[]{x,y-1});
-        }
-        if(ship.isOpenCell(x,y+1)){
-            openNeighbors.add(new int[]{x,y+1});
-        }
-
-        return openNeighbors;
-    }
-
-    // Returns the number of burningCells neighboring a given cell
-    public int numOfBurningCells(int x, int y){
-        int burningCount = 0;
-        if(isBurning(x-1,y) || isBurning(x+1,y) || 
-            isBurning(x,y-1) || isBurning(x,y+1)){
-            burningCount++;
-        }
-        return burningCount;
+    //Finds all the open cells, starts the fire at random open cell
+    public Cell startFire(Ship ship) {
+        Cell fireStartCell = new Cell (random.nextInt(ship.getSize()-1), random.nextInt(ship.getSize()-1));
+        //ship.setCellOnFire(fireStartCell.getX(), fireStartCell.getY());
+        System.out.println("Starting Fire Cell: "+ fireStartCell.getX() + ", " + fireStartCell.getY());
+        return fireStartCell;
     }
 
     /*
@@ -61,36 +31,32 @@ public class Fire {
      *  q = [0,1] defining flammability
      *  K = # of currently burning neighbors of the cell
      */
-    public void spreadFire(){
-        List<int[]> newBurningCells = new ArrayList<>();
+    // public void spreadFire(Ship ship){
+    //     List<int[]> newBurningCells = new ArrayList<>();
 
-        for(int[] cell : burningCells){
-            int x = cell[0];
-            int y = cell[1];
-            List<int[]> neighbors = getOpenNeighbors(x, y);
+    //     for(int[] cell : burningCells){
+    //         int x = cell[0];
+    //         int y = cell[1];
+    //         List<int[]> neighbors = ship.getOpenNeighbors(x, y);
 
-            for(int[] neighbor : neighbors){
-                int xneighbor = neighbor[0];
-                int yneighbor = neighbor[1];
-                if(!isBurning(xneighbor, yneighbor)){
-                    int k = numOfBurningCells(xneighbor, yneighbor);
-                    if(random.nextDouble() < 1 - Math.pow(1 - q, k)){
-                        newBurningCells.add(new int[]{xneighbor, yneighbor});
-                    }
-                }
-            }
-        }
+    //         for(int[] neighbor : neighbors){
+    //             int xneighbor = neighbor[0];
+    //             int yneighbor = neighbor[1];
+    //             if(!ship.isBurning(xneighbor, yneighbor)){
+    //                 int k = ship.numOfBurningCells(xneighbor, yneighbor);
+    //                 if(random.nextDouble() < 1 - Math.pow(1 - q, k)){
+    //                     newBurningCells.add(new int[]{xneighbor, yneighbor});
+    //                 }
+    //             }
+    //         }
+    //     }
 
-        burningCells.addAll(newBurningCells);
-    }
+    //     burningCells.addAll(newBurningCells);
+    // }
 
-    public boolean isBurning(int x, int y){
-        return ship.getGrid()[x][y].equals("burning");
-    }
-
-    // Ends the fire -- only when button pressed
-    public void endFire(){
-        burningCells.clear();
-    }
+    // // Ends the fire -- only when button pressed
+    // public void endFire(){
+    //     burningCells.clear();
+    // }
 
 }
