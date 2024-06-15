1e1cb1bdb7acb2f8ed9d005f3e51b43e3dfdf941
saanyay <saanyajauhri@gmail.com>
2024-06-15 02:51:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,117 +11,84 @@ public class Main {
     Scanner myObj = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int D = 7; 
-        double q = 0.3; // Fire Spread
-        String menuOption;
+        int D = 5; 
+        String menuOption = "1";
+        // String [][] fixedGrid = {
+        //     {"blocked", "open", "open", "blocked", "open"},
+        //     {"blocked", "open", "blocked", "open", "open"},
+        //     {"blocked", "open", "open", "open", "blocked"},
+        //     {"open", "blocked", "blocked", "open", "open"},
+        //     {"open", "open", "open", "open", "blocked"}
+        // };
+        // Cell button = new Cell(1,0);
+        // Cell bot = new Cell(1,3);
 
-        menuOption = printMenu();
+        //menuOption = printMenu();
 
         while(true){
-            if(menuOption.equals(String.valueOf(1))){ // Bot 1
+            if(menuOption.equals("1")){ // Bot 1
                 // Initialize Ship
                 Ship ship = new Ship(D);
+                //Ship ship = new Ship(fixedGrid, bot, button);
                 //ship.startShip();
-                Bot bot = new Bot(ship);
-                Fire fire = new Fire(ship);
-                Cell botCell = bot.placeBot();
-                ship.printCompleteGrid(botCell, fire);
                 Bot_1 bot_1 = new Bot_1(ship);
+                Cell botLocation = bot_1.placeBot();
+                ship.setCellOnFire(botLocation);
+                //Fire fire = new Fire(ship); <- old line where Fire class was in use
+                ship.setBot(botLocation.getX(), botLocation.getY());
+                ship.printCompleteGrid();
+                        
+                
+                List<Cell> shortestPath = (bot_1.breadthFirstSearch(ship));
+                
+                // System.out.println(shortestPath.size());
+                // for(int i = 0; i < shortestPath.size(); i++){
+                //     Cell cell = shortestPath.get(i);
+                // System.out.println(cell.toString());
+                // }
 
-                List<Cell> shortestPath = (bot_1.breadthFirstSearch(botCell,ship.getButton(), fire));
-                System.out.println(shortestPath.size());
-                for(int i = 0; i < shortestPath.size(); i++){
-                    Cell cell = shortestPath.get(i);
-                System.out.println(cell.toString());
-                }
+                // //fire.startFire(ship);
 
-                //fire.startFire(ship);
+                // for(int i = 0; i < 10; i++){
 
-                for(int i = 0; i < 10; i++){
-
-                    //bot_1.breadthFirstSearch(botCell, buttonCell, fireCell);
-                    bot.moveBot();
-                    System.out.println("Bot position after move " + (i + 1) + ": (" + bot.getX() + ", " + bot.getY() + ")");
-                    ship.printCompleteGrid(botCell, fire);
-                    if((bot.getX() == ship.getButton().getX()) && (bot.getY() == ship.getButton().getY())){
-                        //fire.endFire();
-                        System.out.println("The bot has successfully found the button.\nThe fire has been extinguished.");
-                        break;
-                    }
-                }  
+                //     //bot_1.breadthFirstSearch(botCell, buttonCell, fireCell);
+                //     bot.moveBot();
+                //     System.out.println("Bot position after move " + (i + 1) + ": (" + bot.getX() + ", " + bot.getY() + ")");
+                //     ship.printCompleteGrid(botCell, fire);
+                //     if((bot.getX() == ship.getButton().getX()) && (bot.getY() == ship.getButton().getY())){
+                //         //fire.endFire();
+                //         System.out.println("The bot has successfully found the button.\nThe fire has been extinguished.");
+                //         break;
+                //     }
+                // }  
+                break;
             }
+            
 
             if(menuOption.equals(String.valueOf(2))){ //Bot 2
-                // Initialize Ship
                 Ship ship = new Ship(D);
-                //ship.startShip();
-                Bot bot = new Bot(ship);
-                Fire fire = new Fire(ship);
-                Cell botCell = bot.placeBot();
-                ship.printCompleteGrid(botCell, fire);
                 Bot_2 bot_2 = new Bot_2(ship);
-
-                List<Cell> shortestPath = (bot_2.breadthFirstSearch(botCell,ship.getButton(), fire));
-                System.out.println(shortestPath.size());
-                for(int i = 0; i < shortestPath.size(); i++){
-                    Cell cell = shortestPath.get(i);
-                System.out.println(cell.toString());
-                }
-
-                fire.startFire(ship);
-
-                for(int i = 0; i < 10; i++){
-
-                    //bot_1.breadthFirstSearch(botCell, buttonCell, fireCell);
-                    bot.moveBot();
-                    System.out.println("Bot position after move " + (i + 1) + ": (" + bot.getX() + ", " + bot.getY() + ")");
-                    ship.printCompleteGrid(botCell, fire);
-                    if((bot.getX() == ship.getButton().getX()) && (bot.getY() == ship.getButton().getY())){
-                        //fire.endFire();
-                        System.out.println("The bot has successfully found the button.\nThe fire has been extinguished.");
-                        break;
-                    }
-                }
+                Cell botLocation = bot_2.placeBot();
+                ship.setCellOnFire(botLocation);
+                ship.setBot(botLocation.getX(), botLocation.getY());
+                ship.printCompleteGrid();
+                        
+                
+                List<Cell> shortestPath = (bot_2.breadthFirstSearch(ship));
             }
-            menuOption = printMenu();
-        }
 
-             
-        
-        
-        // Initialize Bot
-        // Bot bot = new Bot(ship);
-        // bot.placeBot();
+            // if(menuOption.equals(String.valueOf(4))){
+            //     Ship ship = new Ship(D);
+            //     Bot_4 bot_4 = new Bot_4(ship);
+            //     Bot bot = new Bot(ship);
+            //     Fire fire = new Fire(ship);
+            //     Cell botCell = bot.placeBot();
+            //     ship.printCompleteGrid(botCell, fire);
 
-        //Initialize Bot 1
-        //Bot_1 bot_1 = new Bot_1(ship);
-        // bot.placeBot();
-
-        // Initialize Fire
-
-        // Fire fire = new Fire(ship);
-        
-
-        // ship.printCompleteGrid(bot, fire, button);
-        // System.out.println();
-
-        //Cell cell = new Cell(bot.getX(), bot.getY());
-        //bot.moveBot(cell);
-
-        //bot_1.breadthFirstSearch(bot.getX(), bot.getY(), button.getXButtonLoc(), button.getYButtonLoc(), fireX, fireY);
-
-        // ship.printCompleteGrid(bot, fire, button);
-        // System.out.println();
-
-        //bot.moveBot();
-
-        // ship.printCompleteGrid(bot, fire);
-            
-        // //Cell buttonCell = new Cell(button.getXButtonLoc(), button.getYButtonLoc());
-        // Cell fireCell = new Cell(fire.startFire(ship).getX(), fire.startFire(ship).getY());
-
-
-        
+            //     bot_4.aStarSearch(ship, botCell, ship.getButton());
+            // }
+            //menuOption = printMenu();
+        }        
     }
 
     private static String printMenu(){
