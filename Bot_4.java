8fc5831c1038b2a9f930aa702a2b676444027111
Kathleen Zhou <kzhou8@u.rochester.edu>
2024-06-16 14:03:19 -0400

    Update Bot_4.java

--- Bot_4.java
+++ Bot_4.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -74,8 +75,75 @@ public class Bot_4 extends Bot{
                 }
             }
         }
+        backupBFS(ship);
+        return new ArrayList<>();
+    }
+
+    public List<Cell> backupBFS(Ship ship){
+        Cell bot = new Cell(ship.getBot().getX(), ship.getBot().getY());
+        Cell button = new Cell(ship.getButton().getX(), ship.getButton().getY());
+        Queue <Cell> queue = new LinkedList<>();
+        boolean visited[][] = new boolean[ship.getSize()][ship.getSize()];
+        Cell[][] parent = new Cell[ship.getSize()][ship.getSize()];
+        Cell[][] cells = new Cell[ship.getSize()][ship.getSize()];
+        for (int i = 0; i < ship.getSize(); i++) {
+            for (int j = 0; j < ship.getSize(); j++) {
+                if (ship.isOpenCell(i,j)) {
+                    cells[i][j] = new Cell(i, j, Integer.MAX_VALUE, null);
+                }
+                visited[i][j] = false;
+            }
+        }
+
+        //Starting Node
+        queue.add(cells[bot.getX()][bot.getY()]);
+        visited[bot.getX()][bot.getY()] = true;
+        parent[bot.getX()][bot.getY()] = null;
+        Cell current = null;
+        Cell previous = null;
+
+        while(!queue.isEmpty()){
+
+            // Assign current node & Moves to next cell in queue
+            previous = current;
+            current = queue.poll();
+
+            // Check if bot = button
+            if(current.equals(button)){
+                List<Cell> path = new ArrayList<>();
+                Cell node = current;
+                if (node.getParentCell() == null || node == null) {
+                    System.out.println("No path exists between bot and button.");
+                    return null;
+                }
+                else {
+                    while(node.getParentCell() != null){
+                        path.add(node);
+                        node = node.getParentCell();
+                    }
+                    Collections.reverse(path);
+                    return path;
+                }
+            }
+            //Add children of bot to queue
+            else{
+                for(Cell neighbor : getBotNeighbors(current)){
+                    if((!(ship.isBurning(neighbor.getX(),neighbor.getY()))) &&
+                            (neighbor.isOpenCell(ship)) &&
+                            visited[neighbor.getX()][neighbor.getY()] == false){
+                        int dist = current.getDist() + 1;
+                        if (dist < neighbor.getDist()) {
+                            visited[neighbor.getX()][neighbor.getY()] = true;
+                            cells[neighbor.getX()][neighbor.getY()].setDist(dist);
+                            cells[neighbor.getX()][neighbor.getY()].setParentCell(cells[current.getX()][current.getY()]);
+                            queue.add(cells[neighbor.getX()][neighbor.getY()]);
+                        }
+                    }
+                }
+            }
+        }
         System.out.println("No path exists between bot and button.");
         return new ArrayList<>();
     }
-    
+
 }
