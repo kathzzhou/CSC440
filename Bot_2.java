e04d089634a109f44fb43db5895ce6606efd74ce
Kathleen Zhou <kzhou8@u.rochester.edu>
2024-06-16 20:52:26 -0400

    Update Bot_2.java

--- Bot_2.java
+++ Bot_2.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,14 +22,14 @@ public class Bot_2 extends Bot{
         Cell[][] parent = new Cell[ship.getSize()][ship.getSize()];
         Cell[][] cells = new Cell[ship.getSize()][ship.getSize()];
         for (int i = 0; i < ship.getSize(); i++) {
-	        for (int j = 0; j < ship.getSize(); j++) {
-	            if (ship.isOpenCell(i,j)) {
-	                cells[i][j] = new Cell(i, j, Integer.MAX_VALUE, null);
-	            }
+            for (int j = 0; j < ship.getSize(); j++) {
+                if (ship.isOpenCell(i,j)) {
+                    cells[i][j] = new Cell(i, j, Integer.MAX_VALUE, null);
+                }
                 visited[i][j] = false;
-	        }
-	    }
-      
+            }
+        }
+
         //Starting Node
         queue.add(cells[bot.getX()][bot.getY()]);
         visited[bot.getX()][bot.getY()] = true;
@@ -36,7 +37,7 @@ public class Bot_2 extends Bot{
         Cell current = null;
         Cell previous = null;
 
-        while(!queue.isEmpty()){        
+        while(!queue.isEmpty()){
 
             // Assign current node & Moves to next cell in queue
             previous = current;
@@ -56,16 +57,15 @@ public class Bot_2 extends Bot{
                         node = node.getParentCell();
                     }
                     Collections.reverse(path);
-
                     return path;
                 }
             }
             //Add children of bot to queue
             else{
                 for(Cell neighbor : getBotNeighbors(current)){
-                    if((!(ship.isBurning(neighbor.getX(),neighbor.getY()))) && 
-                        (neighbor.isOpenCell(ship)) && 
-                        visited[neighbor.getX()][neighbor.getY()] == false){
+                    if((!(ship.isBurning(neighbor.getX(),neighbor.getY()))) &&
+                            (neighbor.isOpenCell(ship)) &&
+                            visited[neighbor.getX()][neighbor.getY()] == false){
                         int dist = current.getDist() + 1;
                         if (dist < neighbor.getDist()) {
                             visited[neighbor.getX()][neighbor.getY()] = true;
@@ -80,5 +80,5 @@ public class Bot_2 extends Bot{
         System.out.println("No path exists between bot and button.");
         return new ArrayList<>();
     }
-    
+
 }
